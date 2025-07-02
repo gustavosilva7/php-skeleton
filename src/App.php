e429bcec742fb97fe4e9d7fc5bdd343395e0ba0d
gustavosilva7 <gustavosilvaalexgusta007@gmail.com>
2025-07-01 22:43:25 -0300

    style: runnig fix

--- src/App.php
+++ src/App.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App;
 
-class App{
+class App
+{
     public function hello(): string
-    {return 'Hello, PHP Skeleton!';
-    }}
+    {
+        return 'Hello, PHP Skeleton!';
+    }
+}
